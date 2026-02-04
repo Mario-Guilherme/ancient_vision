3b9ef21c4be05a2b53bc18e936f6a71c85090a3a
mario guilherme <carvalho16car@gmail.com>
2026-02-04 00:47:30 -0400

    feat: add test highpass and sharpen

--- vision-hw1/tryit.py
+++ vision-hw1/tryit.py
@@ -16,3 +16,19 @@ im = load_image("data/dog.jpg")
 f = make_box_filter(7)
 blur = convolve_image(im, f, 1)
 save_image(blur, "dog-box7")
+
+
+im = load_image("data/dog.jpg")
+f = make_highpass_filter()
+highpass = convolve_image(im, f, 0)
+image_abs(highpass)
+image_normalize(highpass)
+save_image(highpass, "dog-highpass")
+
+
+im = load_image("data/dog.jpg")
+f = make_sharpen_filter()
+sharpen = convolve_image(im, f, 1)
+image_abs(sharpen)
+# image_normalize(sharpen)
+save_image(sharpen, "dog-sharpen")
