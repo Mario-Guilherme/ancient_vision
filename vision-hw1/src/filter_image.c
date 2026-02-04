7bde90bd1a1d187bb7af3fd33d03181ce685d204
mario guilherme <carvalho16car@gmail.com>
2026-02-03 22:23:02 -0400

    feat: add convolve image

--- vision-hw1/src/filter_image.c
+++ vision-hw1/src/filter_image.c
@@ -7,32 +7,33 @@
 #define TWOPI 6.2831853
 
 image image_of_ones(int h,int w,int c){
-    image im = make_image(h,w,c);
+    image im = make_image(w,h,c);
     for (int i = 0; i < im.h*im.w*im.c; i++)
     {
 
-        im.data[i] = 1;
+        im.data[i] = 1.0f;
     }
     return  im;
 }
 
 void l1_normalize(image im)
 {
-    int normalize;
+    float normalize=0;
     
 
     for (int i = 0; i < im.c*im.h*im.w; i++)
     {
         normalize += im.data[i];
     }
-    
+    if (normalize==0)
+        return;
     for (int i = 0; i < im.h; i++)
     {
-        for (int j = 0; j < im.w; i++)
+        for (int j = 0; j < im.w; j++)
         {
            for (int z = 0; z < im.c; z++)
            {
-            set_pixel(im,i,j,z,1/normalize);  
+            set_pixel(im,i,j,z,(float)1/normalize);  
            }
            
         }
@@ -50,10 +51,65 @@ image make_box_filter(int w)
 
 image convolve_image(image im, image filter, int preserve)
 {
-    assert(im.c == filter.c);
-    // TODO
-    return make_image(1,1,1);
+    // cross correlação discreta pode ser expressa
+    // g(x,y) = h⊗f = ∑h(u,v)f(i+u,j+v), seja h(u,v) o kernel que se deseja aplicar e f(x,y) o pixel da imagem original
+    // g(x,y) é o pixel aplicado a mascara(kernel)
+    // get_pixel está usando a estragédia clamp
+    // também é necessário centralizar o centro do kernel com o pixel selecionado, então é preciso pegar as coordenadas
+    // do centro do kernel e assim aplicar a cross correlation
+
+    assert(im.c == filter.c || filter.c == 1);
+
+    int out_c = preserve ? im.c : 1;
+
+    image out = make_image(im.w, im.h, out_c);
+
+    int fh = filter.h / 2;
+    int fw = filter.w / 2;
+
+    for (int i = 0; i < im.w; i++)
+    {
+        for (int j = 0; j < im.h; j++)
+        {
+            for (int z = 0; z < im.c; z++)
+            {
+                float sum = 0.0f;
+
+                for (int a = 0; a < filter.w; a++)
+                {
+                    for (int b = 0; b < filter.h; b++)
+                    {
+                        int fc = (filter.c == 1) ? 0 : z;
+
+                        float im_val = get_pixel(
+                            im,
+                            i + a - fh,
+                            j + b - fw,
+                            z
+                        );
+
+                        float f_val = get_pixel(filter, a, b, fc);
+
+                        sum += im_val * f_val;
+                    }
+                }
+
+                if (preserve)
+                {
+                    set_pixel(out, i, j, z, sum);
+                }
+                else
+                {
+                    float prev = get_pixel(out, i, j, 0);
+                    set_pixel(out, i, j, 0, prev + sum);
+                }
+            }
+        }
+    }
+
+    return out;
 }
+
 
 image make_highpass_filter()
 {
